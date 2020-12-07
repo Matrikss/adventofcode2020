2f7afc4d15b289fdb3e0b098b23c58d0ab424022
Bruno Loureiro <matrikss@gmail.com>
2020-12-07 19:51:39 +0000

    Add solution for day 7 part 2
    Fugly code

--- day7.py
+++ day7.py
@@ -7,10 +7,19 @@ def find_containers(bag_dict={}, bag=''):
     return result
 
 
+def find_containers_b(bag_dict={}, bag=''):
+    result = 0
+    if bag in bag_dict.keys():
+        for b in bag_dict[bag]:
+            result += int(b[1]) + int(b[1]) * find_containers_b(bag_dict, b[0])
+    return result
+
+
 with open('input7.txt') as f:
     read_data = f.read()
 
     bags = {}
+    bags_b = {}
 
     lines = read_data.split('\n')
     for line in lines:
@@ -26,7 +35,13 @@ with open('input7.txt') as f:
                 bags[clean_content].append(clean_container)
             else:
                 bags[clean_content] = [clean_container]
+            if clean_container in bags_b.keys():
+                bags_b[clean_container].append([clean_content, split_content[1]])
+            else:
+                bags_b[clean_container] = [[clean_content, split_content[1]]]
 
     answer_a = set(find_containers(bags, 'shiny gold'))
+    answer_b = find_containers_b(bags_b, 'shiny gold')
 
     print(len(answer_a))
+    print(answer_b)
