a182dc7b9bc0829cf90b99e60da17a7031ceca14
Bruno Loureiro <matrikss@gmail.com>
2020-12-09 20:53:50 +0000

    Add solution for part 2 day 9

--- day9.py
+++ day9.py
@@ -7,10 +7,29 @@ with open('input9.txt') as f:
 
     lines = read_data.split('\n')
     line_count = len(lines)
+    value_a = None
     i = WINDOW_SIZE
     while i < line_count:
         result = sum_search(lines[i - WINDOW_SIZE:i], lines[i])
         if not result:
-            print(lines[i])
+            value_a = int(lines[i])
             break
         i += 1
+
+    print(value_a)
+
+    i = 0
+    j = 1
+    sum = int(lines[i])
+    while True:
+        sum += int(lines[j])
+        if sum == value_a:
+            numbers = sorted(lines[i:j + 1], key=int)
+            print(int(numbers[0]) + int(numbers[len(numbers) - 1]))
+            break
+        if sum > value_a:
+            i += 1
+            j = i + 1
+            sum = int(lines[i])
+            continue
+        j += 1
