60cafe6a2d2a07013ed3e4244b1273871aee9a25
Bruno Loureiro <matrikss@gmail.com>
2020-12-09 19:55:05 +0000

    Refactor day 1 for usage in day 9

--- day1.py
+++ day1.py
@@ -1,24 +1,32 @@
 TARGET = 2020
 
-with open('input1.txt') as f:
-    read_data = f.read()
 
-    numbers = read_data.split('\n')
+def sum_search(numbers, target):
+    numbers = sorted(numbers, key=int)
     start = 0
     end = len(numbers) - 1
     while True:
+        if start >= end:
+            return None
         sum = int(numbers[start]) + int(numbers[end])
         if sum == TARGET:
-            print(int(numbers[start]) * int(numbers[end]))
-            break
+            return int(numbers[start]) * int(numbers[end])
         if sum < TARGET:
             start += 1
         else:
             end -= 1
 
+
+with open('input1.txt') as f:
+    read_data = f.read()
+
+    numbers = read_data.split('\n')
+    print(sum_search(numbers, TARGET))
+
     start = 0
     middle = 1
     end = len(numbers) - 1
+    numbers = sorted(numbers, key=int)
     while True:
         sum = int(numbers[start]) + int(numbers[middle]) + int(numbers[end])
         if sum == TARGET:
