522ef7d736b75afff264a90734af215417c9dd24
Bruno Loureiro <matrikss@gmail.com>
2020-12-11 20:23:19 +0000

    Add part 2 of day 11

--- day11.py
+++ day11.py
@@ -6,6 +6,29 @@ FLOOR = '.'
 OCCUPIED = '#'
 
 
+def go_in_direction(seat_map, position_i, position_j, size_i, size_j, direction_i, direction_j):
+    if position_i < 0 or position_j < 0 or position_i >= size_i or position_j >= size_j:
+        return EMPTY
+    if seat_map[position_i][position_j] == FLOOR:
+        return go_in_direction(seat_map, position_i + direction_i, position_j + direction_j, size_i, size_j, direction_i, direction_j)
+    return seat_map[position_i][position_j]
+
+
+def check_direction_change(seat_map, position_i, position_j, size_i, size_j):
+    count = {EMPTY: 0, FLOOR: 0, OCCUPIED: 0}
+
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if i == 0 and j == 0:
+                continue
+            count[go_in_direction(seat_map, position_i + i, position_j + j, size_i, size_j, i, j)] += 1
+    if seat_map[position_i][position_j] == EMPTY and count[OCCUPIED] == 0:
+        return OCCUPIED
+    if seat_map[position_i][position_j] == OCCUPIED and count[OCCUPIED] >= 5:
+        return EMPTY
+    return seat_map[position_i][position_j]
+
+
 def check_change(seat_map, position_i, position_j, size_i, size_j):
     count = {EMPTY: 0, FLOOR: 0, OCCUPIED: 0}
 
@@ -13,7 +36,6 @@ def check_change(seat_map, position_i, position_j, size_i, size_j):
         for j in itertools.filterfalse(lambda x: x < 0 or x >= size_j, range(position_j - 1, position_j + 2)):
             if i == position_i and j == position_j:
                 continue
-            # print(i,j)
             count[seat_map[i][j]] += 1
     if seat_map[position_i][position_j] == EMPTY and count[OCCUPIED] == 0:
         return OCCUPIED
@@ -53,7 +75,8 @@ with open('input11.txt') as f:
                 next_seating_status = copy.deepcopy(lines)
                 count = {EMPTY: 0, FLOOR: 0, OCCUPIED: 0}
 
-        change = check_change(lines, i, j, line_count, row_count)
+        # change = check_change(lines, i, j, line_count, row_count)  # part 1
+        change = check_direction_change(lines, i, j, line_count, row_count)  # part 2
         count[change] += 1
         if change != lines[i][j]:
             changes += 1
