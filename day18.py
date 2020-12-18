911296ce6364f82e4889a200cb326c2ba72dc46f
Bruno Loureiro <matrikss@gmail.com>
2020-12-18 20:26:51 +0000

    Day 18 part 2 - only works on examples

--- day18.py
+++ day18.py
@@ -1,31 +1,49 @@
 import re
 
 
-def calculate(start_index=0, expression=['']):
-    result = int(expression[start_index])
+def calculate_line(line, is_part2=False):
+    groups = re.findall('\\(([^()]*)\\)', line)
+    while len(groups) > 0:
+        for group in groups:
+            result = calculate(0, group, is_part2)
+            line = line.replace('(' + group + ')', str(result), 1)
+        groups = re.findall('\\(([^()]*)\\)', line)
+    return calculate(0, line, is_part2)
+
+
+def calculate(start_index=0, expression='', is_part2=True):
+    if is_part2:
+        groups = re.findall('(\\d+ \\+ \\d+)', expression)
+        while len(groups) > 0:
+            for group in groups:
+                expression = expression.replace(group, str(eval(group)), 1)
+            groups = re.findall('(\\d+ \\+ \\d+)', expression)
+        return eval(expression)
+    else:
+        expression_split = expression.split(' ')
+    result = int(expression_split[start_index])
     i = start_index
-    while i < len(expression) - 1:
-        result = eval(str(result) + expression[i + 1] + expression[i + 2])
+    while i < len(expression_split) - 1:
+        result = eval(str(result) + expression_split[i + 1] + expression_split[i + 2])
         i += 2
     return result
 
 
 part_1 = []
+part_2 = []
 
 with open('input18.txt') as f:
     read_data = f.read()
     lines = read_data.split('\n')
 
     for line in lines:
-        groups = re.findall('\\(([^()]*)\\)', line)
-        while len(groups) > 0:
-            for group in groups:
-                result = calculate(0, group.split(' '))
-                line = line.replace('(' + group + ')', str(result))
-            groups = re.findall('\\(([^()]*)\\)', line)
-
-        expression = line.split(' ')
-        result = calculate(0, expression)
-        part_1.append(result)
+        result_1 = calculate_line(line, False)
+        result_2 = calculate_line(line, True)
+        part_1.append(result_1)
+        part_2.append(result_2)
 
     print(sum(part_1))
+    print(sum(part_2))
+
+# 65663464359529 too high
+# 65663464313197 too high
