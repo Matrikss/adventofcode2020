c79a5b104a97ae2de844bbb7d5819d477c94afe3
Bruno Loureiro <matrikss@gmail.com>
2020-12-19 11:46:48 +0000

    Day 19 part 2

--- day19.py
+++ day19.py
@@ -5,7 +5,14 @@ MESSAGES = 2
 COMPLETE_RULE = re.compile('[a-z\\|\\(\\)\\s]+')
 
 
-def construct_expression(raw_r={}):
+def construct_expression(raw_r={}, is_part_2=False):
+    if is_part_2:
+        magic_number = {'8': 3, '11': 2}
+        raw_r['8'] = ' 42 | 42 8'
+        raw_r['11'] = ' 42 31 | 42 11 31'
+    else:
+        magic_number = {}
+
     leaf_rules = {key: value.replace('"', '').rstrip().lstrip() for (key, value) in raw_r.items() if '"' in value}
     while len(leaf_rules.keys()) < len(raw_r.keys()):
         for key, value in raw_r.items():
@@ -18,11 +25,19 @@ def construct_expression(raw_r={}):
                         replaced = re.sub('\\b' + rule_id + '\\b', rule_value, replaced)
                 if COMPLETE_RULE.fullmatch(replaced):
                     leaf_rules[key] = replaced
+                if key in magic_number:
+                    if COMPLETE_RULE.fullmatch(re.sub('\\b' + key + '\\b', '', replaced)):
+                        if magic_number[key] == 1:
+                            leaf_rules[key] = re.sub('\\b' + key + '\\b', '(' + re.sub('\\b' + key + '\\b', '', replaced) + ')', replaced)
+                        else:
+                            raw_r[key] = re.sub('\\b' + key + '\\b', '(' + replaced + ')', replaced)
+                            magic_number[key] -= 1
 
     return leaf_rules['0'].replace(' ', '')
 
 
 valid_part1 = 0
+valid_part2 = 0
 raw_rules = {}
 
 with open('input19.txt') as f:
@@ -33,14 +48,18 @@ with open('input19.txt') as f:
     for line in lines:
         if line == '':
             mode = MESSAGES
-            compiled_expression = re.compile(construct_expression(raw_rules))
+            compiled_expression1 = re.compile(construct_expression(raw_rules, False))
+            compiled_expression2 = re.compile(construct_expression(raw_rules, True))
             continue
         if mode == RULES:
             line_split = line.split(':')
             raw_rules[line_split[0]] = line_split[1]
             continue
         if mode == MESSAGES:
-            if compiled_expression.fullmatch(line):
+            if compiled_expression1.fullmatch(line):
                 valid_part1 += 1
+            if compiled_expression2.fullmatch(line):
+                valid_part2 += 1
 
 print(valid_part1)
+print(valid_part2)
